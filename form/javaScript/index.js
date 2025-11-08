cc1e512e6852d69773a52ad0633021bf7537952f
riyyan-optiverse <riyyanoptiverse@gmail.com>
2025-11-08 11:45:38 +0500

    learned about JS

---
+++ form/javaScript/index.js
@@ -0,0 +1,67 @@
+/*const studant={
+     fullName:"Ali",
+     age:16,
+     percentage:80,
+     isPass:true, 
+}; */
+   /*studant["age"]=studant["age"]+2
+   console.log(studant["age"]);
+   let a = 5;
+   let b = 4;
+   console.log("a = ", a, " & b = ", b);
+   console.log("a = ", a + b );
+   console.log("a = ", a - b );
+   console.log("a = ", a * b );
+   console.log("a = ", a / b ); */
+
+  /* let a = 5;
+   let b = 4;
+       console.log("a = ", a, " & b = ", b);
+       
+       console.log("a++ =" , a++);
+       console.log("a =" , a);
+       console.log("++a =" , ++a); */
+
+    /*   let a = 5;
+      let b = 4;
+       console.log("a = ", a, " & b = ", b);
+       console.log("a-- =" , a--);
+       console.log("a =" , a);
+       console.log("--a =" , --a) */
+
+
+     /*  let a = 5;
+       let b = 4;
+       a += 5; 
+       console.log("a =" , a); */
+
+    /*  let a = 5;
+       let b = 5;
+       console.log("5 == 5" , a == b);
+        console.log("5 != 5" , a != b); */
+     /*   let age = 16;
+        if (age >= 18) {
+        console.log("You can vote");
+        }
+        if (age < 18){
+          console.log("You cannot vote");
+        } */
+     /*  for(let i = 1; i <= 7; i++){
+          console.log("Hello Everyone");
+       } */
+   /*   for(let i = 1; i <= 7; i++){
+          console.log("i =", i);
+      } */
+   /*  let i = 1;
+     while(i <= 7) {
+      console.log("i =", i);
+      i++;
+     } */
+  /*   let i = 1;
+     do{
+       console.log("i =", i);
+       i++;
+     } while(i <= 6); */
+      for(let i = 0; i <=100; i++){
+        console.log("i =", i);
+      }
